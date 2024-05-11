ae16ac1d4f452be8f3a695011d02ea1989284fc3
TAKEO <mr.thetakeo@gmail.com>
2024-05-11 23:04:50 +0900

    add code

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using AWSBedrockSample;
+
+Console.Write("モデルに質問する場合は1と入力してください: ");
+var isModel = Console.ReadLine() == "1";
+
+Console.Write("質問を入力してください: ");
+var userQuestion = Console.ReadLine();
+
+string answer;
+
+if (isModel)
+{ answer = await UseAWSBedrock.InvokeModelAsync(userQuestion); }
+else
+{ answer = await UseAWSBedrock.InvokeAgentAsync(userQuestion); }
+
+Console.WriteLine("回答: " + answer);
+Console.ReadKey();
---
+++ UseAWSBedrock.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.IO;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.Util;
+using Amazon.BedrockRuntime;
+using Amazon.BedrockRuntime.Model;
+using Amazon.BedrockAgentRuntime;
+using Amazon.BedrockAgentRuntime.Model;
+
+namespace AWSBedrockSample
+{
+    internal class UseAWSBedrock
+    {
+        /// <summary>
+        /// モデルにクエリを送信する。
+        /// </summary>
+        /// <param name="prompt">プロンプト</param>
+        /// <returns></returns>
+        public static async Task<string> InvokeModelAsync(string prompt)
+        {
+            var enclosedPrompt = "\n\nHuman:" + prompt + "\n\nAssistant:";
+            var payload = new JsonObject
+                {
+                    { "prompt",enclosedPrompt },
+                    { "max_tokens_to_sample",4000 },
+                    { "temperature",0.5 }
+                    //{ "stop_sequences",new JsonArray("\n\nHuman:") }
+                }.ToJsonString();
+
+            var request = new InvokeModelRequest
+            {
+                ModelId = "anthropic.claude-v2:1",
+                Body = AWSSDKUtils.GenerateMemoryStreamFromString(payload),
+                ContentType = "application/json",
+                Accept = "application/json"
+            };
+
+            var bedrockClient = new AmazonBedrockRuntimeClient(Credentials.Credential, RegionEndpoint.USEast1);
+            try
+            {
+                var response = await bedrockClient.InvokeModelAsync(request);
+                if (response.HttpStatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    using var reader = new StreamReader(response.Body);
+                    return await reader.ReadToEndAsync();
+                }
+                else
+                {
+                    return "Model invocation failed with status code: " + response.HttpStatusCode;
+                }
+            }
+            catch (Exception e)
+            {
+                return "Error invoking model: " + e.Message;
+            }
+        }
+
+
+        /// <summary>
+        /// ナレッジベースにクエリを送信する。
+        /// </summary>
+        /// <param name="prompt">プロンプト</param>
+        /// <returns></returns>
+        public static async Task<string> InvokeAgentAsync(string prompt)
+        {
+            var bedrockAgentClient = new AmazonBedrockAgentRuntimeClient(Credentials.Credential, RegionEndpoint.USEast1);
+
+            var invokeAgentRequest = new InvokeAgentRequest
+            {
+                AgentId = AgentInfo.AgentId,
+                AgentAliasId = AgentInfo.AgentAliasId,
+                InputText = prompt,
+                SessionId = Guid.NewGuid().ToString()
+            };
+
+            try
+            {
+                var response = await bedrockAgentClient.InvokeAgentAsync(invokeAgentRequest);
+                if (response.HttpStatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    // ToDo:forerachの流用ではなく、直接レスポンスを取得できるようにする。
+                    foreach (Amazon.BedrockAgentRuntime.Model.PayloadPart item in response.Completion)
+                    {
+                        using var reader = new StreamReader(item.Bytes);
+                        return await reader.ReadToEndAsync();
+                    }
+
+                    // ここには来ない想定。
+                    return "Error response";
+                }
+                else
+                {
+                    return "Agent invocation failed with status code: " + response.HttpStatusCode;
+                }
+            }
+            catch (Exception ex)
+            {
+                return $"Error invoking Agent: {ex.Message}";
+            }
+        }
+    }
+}
